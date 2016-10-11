d1e8aee0792d445da936e2e74726e625adf79a66
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-10-12 07:55:21 +0900

    historiesをjsonを返す用に変更

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,74 +1,48 @@
 class HistoriesController < ApplicationController
-  before_action :set_history, only: [:show, :edit, :update, :destroy]
+  before_action :set_history, only: %i(show update destroy)
 
   # GET /histories
-  # GET /histories.json
   def index
     @histories = History.all
+    render formats: :json
   end
 
   # GET /histories/1
-  # GET /histories/1.json
   def show
-  end
-
-  # GET /histories/new
-  def new
-    @history = History.new
-  end
-
-  # GET /histories/1/edit
-  def edit
+    render formats: :json
   end
 
   # POST /histories
-  # POST /histories.json
   def create
     @history = History.new(history_params)
-
-    respond_to do |format|
-      if @history.save
-        format.html { redirect_to @history, notice: 'History was successfully created.' }
-        format.json { render :show, status: :created, location: @history }
-      else
-        format.html { render :new }
-        format.json { render json: @history.errors, status: :unprocessable_entity }
-      end
+    if @history.save
+      render :show, status: :created, location: @history
+    else
+      render json: @history.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /histories/1
-  # PATCH/PUT /histories/1.json
   def update
-    respond_to do |format|
-      if @history.update(history_params)
-        format.html { redirect_to @history, notice: 'History was successfully updated.' }
-        format.json { render :show, status: :ok, location: @history }
-      else
-        format.html { render :edit }
-        format.json { render json: @history.errors, status: :unprocessable_entity }
-      end
+    if @history.update(history_params)
+      render :show, status: :ok, location: @history
+    else
+      render json: @history.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /histories/1
-  # DELETE /histories/1.json
   def destroy
     @history.destroy
-    respond_to do |format|
-      format.html { redirect_to histories_url, notice: 'History was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_history
-      @history = History.find(params[:id])
-    end
+  def set_history
+    @history = History.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def history_params
-      params.require(:history).permit(:date, :team, :result, :starter, :location)
-    end
+  def history_params
+    params.require(:history).permit(:date, :team, :result, :starter, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
-  resources :histories
+  resources :histories, only: %i(index show create update destroy)
 end
