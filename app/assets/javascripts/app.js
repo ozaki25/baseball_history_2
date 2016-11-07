3f1fc6657ab8968289eeb678d8c0dd8ad062b135
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-11-08 08:20:09 +0900

    modelにデフォルト値を追加

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -55,6 +55,13 @@ var Backbone = require('backbone');
 var moment = require('moment');
 
 module.exports = Backbone.Model.extend({
+    defaults: {
+        date: '',
+        team: '',
+        result: '',
+        starter: '',
+        location: '',
+    },
     getDetailUrl: function() {
         var date = moment(new Date(this.get('date'))).format('YYYYMMDD')
         return 'http://www.fighters.co.jp/gamelive/result/' + date + '01/'
